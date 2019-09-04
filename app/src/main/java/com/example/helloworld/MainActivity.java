320bbdb5db1abcc58c46cad06b51c59e01ad8bda
yechan0704 <key07040@naver.com>
2019-09-04 20:32:33 +0900

    1st

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private RecyclerAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        getData();
+    }
+
+    private void init(){
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new RecyclerAdapter();
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void getData(){
+        List<String> listTitle = Arrays.asList("국화", "사막", "수국", "해파리", "코알라", "등대", "펭귄", "튤립",
+                "국화", "사막", "수국", "해파리", "코알라", "등대", "펭귄", "튤립");
+        List<String> listContent = Arrays.asList(
+                "이 꽃은 국화입니다.",
+                "여기는 사막입니다.",
+                "이 꽃은 수국입니다.",
+                "이 동물은 해파리입니다.",
+                "이 동물은 코알라입니다.",
+                "이것은 등대입니다.",
+                "이 동물은 펭귄입니다.",
+                "이 꽃은 튤립입니다.",
+                "이 꽃은 국화입니다.",
+                "여기는 사막입니다.",
+                "이 꽃은 수국입니다.",
+                "이 동물은 해파리입니다.",
+                "이 동물은 코알라입니다.",
+                "이것은 등대입니다.",
+                "이 동물은 펭귄입니다.",
+                "이 꽃은 튤립입니다."
+        );
+        List<Integer> listResId = Arrays.asList(
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background,
+                R.drawable.ic_launcher_background
+        );
+        for (int i =0;i<listTitle.size();i++){
+            Data data = new Data();
+            data.setTitle(listTitle.get(i));
+            data.setContent(listContent.get(i));
+            data.setResId(listResId.get(i));
+
+            adapter.addItem(data);
+        }
+        adapter.notifyDataSetChanged();
+    }
+}
